7671ccfba5324749b88b716e205bdd86c5db68c8
nader rabhi <naderrabhi93@outlook.com>
2023-01-27 17:20:51 +0100

    adding swagger

--- src/main/java/com/example/jwt/controller/AuthenticationController.java
+++ src/main/java/com/example/jwt/controller/AuthenticationController.java
@@ -4,29 +4,36 @@ import com.example.jwt.request.AuthenticationRequest;
 import com.example.jwt.request.RegisterRequest;
 import com.example.jwt.response.AuthenticationResponse;
 import com.example.jwt.service.AuthenticationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api")
+@Api(value = "Authentication")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService service;
 
+    @ApiOperation(value = "Register User")
     @PostMapping("/register")
+
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) {
         return ResponseEntity.ok(service.register(request));
     }
+
+
+    @ApiOperation(value = "Authenticate user")
     @PostMapping("/authenticate")
+
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
     }
+
 }
